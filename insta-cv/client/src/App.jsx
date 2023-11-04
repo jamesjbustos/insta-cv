9ac3df4981eb033133ff032133725c81f36aa717
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-11-04 01:48:48 -0400

    Define routes for app

--- insta-cv/client/src/App.jsx
+++ insta-cv/client/src/App.jsx
@@ -1,10 +1,17 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AuthHandler from "./components/AuthHandler";
+import Dashboard from "./pages/Dashboard";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/auth-handler" element={<AuthHandler />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 

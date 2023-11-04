98a88b1abf44993d6ca28f457f983550a57cf529
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-11-04 01:49:49 -0400

    Update proxy target

--- insta-cv/client/vite.config.js
+++ insta-cv/client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://localhost:3000",
       },
     },
   },

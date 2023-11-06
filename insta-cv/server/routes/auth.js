af22f02a045f4f1754233f65c1771aef53855182
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-11-06 15:08:40 -0500

    Update auth routes

--- insta-cv/server/routes/auth.js
+++ insta-cv/server/routes/auth.js
@@ -3,52 +3,42 @@ import passport from "passport";
 
 const router = express.Router();
 
-// Login success route
 router.get("/login/success", (req, res) => {
   if (req.user) {
     res.status(200).json({ success: true, user: req.user });
-  } else {
-    // If req.user is not set, it means the user is not logged in.
-    res.status(403).json({ success: false, message: "User not authenticated" });
   }
 });
 
-// Login failed route
 router.get("/login/failed", (req, res) => {
-  res.status(401).json({ success: false, message: "Login failed" });
+  res.status(401).json({ success: true, message: "failure" });
 });
 
-// Logout route
 router.get("/logout", (req, res, next) => {
   req.logout((err) => {
     if (err) {
-      return next(err);
+      return next(error);
     }
 
     req.session.destroy((err) => {
-      if (err) {
-        return next(err);
-      }
       res.clearCookie("connect.sid");
-      res.json({ status: "Success", user: null });
+      res.json({ status: "logout", user: {} });
     });
   });
 });
 
-// GitHub authentication route
 router.get(
   "/github",
-  passport.authenticate("github", { scope: ["read:user"] })
-);
-
-// GitHub callback route
-router.get(
-  "/github/callback",
   passport.authenticate("github", {
-    successRedirect: "/auth/login/success",
-    failureRedirect: "/auth/login/failed",
+    scope: ["read:user"],
   })
 );
 
-// Export the router
+router.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+
 export default router;

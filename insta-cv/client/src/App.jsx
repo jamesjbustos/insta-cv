6b4950de8cf2cffbd9678dce87d5c3228e3050d8
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-11-13 19:10:59 -0500

    Add UserProvider

--- insta-cv/client/src/App.jsx
+++ insta-cv/client/src/App.jsx
@@ -1,14 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import Home from "./pages/Home/Home";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { UserProvider } from '@/context/UserContext';
+import Dashboard from '@/pages/Dashboard';
+import Home from '@/pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <UserProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </UserProvider>
     </BrowserRouter>
   );
 }

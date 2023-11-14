8e2a7ae2476dd3103e08c292bca0665f2cd2145a
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-11-13 19:09:45 -0500

    Tailwind css setup

--- insta-cv/client/vite.config.js
+++ insta-cv/client/vite.config.js
@@ -1,17 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
-    outDir: "../server/public",
+    outDir: '../server/public',
     emptyOutDir: true,
   },
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
+      '/api': {
+        target: 'http://localhost:3000',
       },
     },
   },

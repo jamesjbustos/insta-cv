94b907b3a5f5ce07cb62b0351aeea0f36d4ac43d
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-11-18 12:11:57 -0500

    Remove user context

--- insta-cv/client/src/App.jsx
+++ insta-cv/client/src/App.jsx
@@ -1,18 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { UserProvider } from '@/context/UserContext';
 import Dashboard from '@/pages/Dashboard';
 import Home from '@/pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
-      <UserProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/resume/edit/:resumeId" element={<Dashboard />} />
-        </Routes>
-      </UserProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/resume/edit/:resumeId" element={<Dashboard />} />
+      </Routes>
     </BrowserRouter>
   );
 }

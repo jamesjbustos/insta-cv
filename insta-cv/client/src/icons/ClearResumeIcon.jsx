c1ca508e0442eeb53b695d2b6a73a0d7e028d5f3
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-11-18 00:09:22 -0500

    Add icons

---
+++ insta-cv/client/src/icons/ClearResumeIcon.jsx
@@ -0,0 +1,20 @@
+const ClearResumeIcon = () => (
+  <svg
+    className=" h-5 w-5 text-red-600"
+    fill="none"
+    height="24"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    width="24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M20 5H9l-7 7 7 7h11a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2Z" />
+    <line x1="18" x2="12" y1="9" y2="15" />
+    <line x1="12" x2="18" y1="9" y2="15" />
+  </svg>
+);
+
+export default ClearResumeIcon;
---
+++ insta-cv/client/src/icons/SaveResumeIcon.jsx
@@ -0,0 +1,20 @@
+const SaveResumeIcon = () => (
+  <svg
+    className=" h-4 w-4"
+    fill="none"
+    height="24"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    width="24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z" />
+    <polyline points="17 21 17 13 7 13 7 21" />
+    <polyline points="7 3 7 8 15 8" />
+  </svg>
+);
+
+export default SaveResumeIcon;

7b95d763b760c2e242cb1c5943d7fb7e40433592
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-11-17 22:58:21 -0500

    update resume template

--- insta-cv/server/generator/templates/template1/index.js
+++ insta-cv/server/generator/templates/template1/index.js
@@ -1,138 +1,138 @@
-// template1/index.js
-import { stripIndent, source } from 'common-tags';
+import { stripIndent } from 'common-tags';
 
 const template1 = (data) => {
-  // Define individual section generators
+  // Helper function to join non-empty strings with a separator
+  const joinNonEmpty = (strings, separator = '') =>
+    strings.filter(Boolean).join(separator);
+
+  // Profile Section
   const profileSection = (basics) => {
     if (!basics) return '';
     const { name, email, phone, location, website } = basics;
     const address = location?.address || '';
-    let profileLines = name ? `\\Huge \\scshape {${name}}` : '';
-    profileLines += `\\\\${[address, email, phone, website]
-      .filter(Boolean)
-      .join(' $\\cdot$ ')}\\\\`;
-    return stripIndent`
-      %==== Profile ====%
-      \\vspace*{-10pt}
-      \\begin{center}
-        ${profileLines}
-      \\end{center}
-      \\vspace{2mm}
-    `;
+    const profileDetails = joinNonEmpty(
+      [address, email, phone, website],
+      ' · '
+    );
+
+    return name || profileDetails
+      ? stripIndent`
+        %==== Profile ====%
+        \\vspace*{-10pt}
+        \\begin{center}
+          {\\Huge \\textbf{${name || ''}}} % Name in a larger, bold font
+          \\rule{\\textwidth}{1pt} % Horizontal line
+          {\\normalsize ${profileDetails}} % Contact details in a normal size font
+        \\end{center}
+        \\vspace{2mm}
+      `
+      : '';
   };
 
+  // Education Section
   const educationSection = (education) => {
     if (!education || education.length === 0) return '';
-    return stripIndent`
-      %==== Education ====%
-      \\section*{Education}
-      ${education
-        .map((school) => {
-          const {
-            institution,
-            location,
-            studyType,
-            area,
-            gpa,
-            startDate,
-            endDate,
-          } = school;
-          return stripIndent`
-          \\textbf{${institution}} \\hfill ${location}\\\\
-          ${studyType} in ${area}, GPA: ${gpa} \\hfill ${startDate} - ${endDate}
-          \\vspace{2mm}
-        `;
-        })
-        .join('\n')}
-    `;
+    const educationContent = education
+      .map((school) => {
+        const {
+          institution,
+          location,
+          studyType,
+          area,
+          gpa,
+          startDate,
+          endDate,
+        } = school;
+        return stripIndent`
+        \\textbf{${institution || ''}} \\hfill ${location || ''}\\\\
+        ${studyType || ''} in ${area || ''}${
+          gpa ? `, GPA: ${gpa}` : ''
+        } \\hfill ${startDate || ''} - ${endDate || ''}
+        \\vspace{2mm}
+      `;
+      })
+      .join('\n');
+    return educationContent ? `\\section*{Education}\n${educationContent}` : '';
   };
 
+  // Work Section
   const workSection = (work) => {
     if (!work || work.length === 0) return '';
-    return stripIndent`
-      %==== Work Experience ====%
-      \\section*{Experience}
-      ${work
-        .map((job) => {
-          const {
-            company,
-            position,
-            location,
-            startDate,
-            endDate,
-            highlights,
-          } = job;
-          return stripIndent`
-          \\textbf{${company}} \\hfill ${location}\\\\
-          \\textit{${position}} \\hfill ${startDate} - ${endDate || 'Present'}
-          \\begin{itemize}
-            ${highlights.map((highlight) => `\\item ${highlight}`).join('\n')}
-          \\end{itemize}
-          \\vspace{2mm}
-        `;
-        })
-        .join('\n')}
-    `;
+    const workContent = work
+      .map((job) => {
+        const { company, position, location, startDate, endDate, highlights } =
+          job;
+        const highlightsContent =
+          highlights && highlights.length > 0
+            ? `\\begin{itemize}\n${highlights
+                .map((highlight) => `\\item ${highlight}`)
+                .join('\n')}\n\\end{itemize}`
+            : '';
+        return stripIndent`
+        \\textbf{${company || ''}} \\hfill ${location || ''}\\\\
+        \\textit{${position || ''}} \\hfill ${startDate || ''} - ${
+          endDate || 'Present'
+        }
+        ${highlightsContent}
+        \\vspace{2mm}
+      `;
+      })
+      .join('\n');
+    return workContent ? `\\section*{Experience}\n${workContent}` : '';
   };
 
+  // Skills Section
   const skillsSection = (skills) => {
     if (!skills || skills.length === 0) return '';
     return stripIndent`
-      %==== Skills ====%
-      \\section*{Skills}
-      \\begin{tabular}{ll}
-      ${skills
-        .map((skill) => {
-          const { name, keywords } = skill;
-          return `${name}: & ${keywords.join(', ')} \\\\`;
-        })
-        .join('\n')}
-      \\end{tabular}
-      \\vspace{2mm}
-    `;
+    %==== Skills ====%
+    \\section*{Skills}
+    \\begin{tabular}{ll}
+    ${skills
+      .map((skill) => {
+        return `${skill.name}: & ${skill.keywords.join(', ')}`;
+      })
+      .join(' \\\\ ')} % End each table row with a new line in LaTeX
+    \\end{tabular}
+    \\vspace{2mm}
+  `;
   };
 
+  // Projects Section
   const projectsSection = (projects) => {
     if (!projects || projects.length === 0) return '';
-    return stripIndent`
-      %==== Projects ====%
-      \\section*{Projects}
-      ${projects
-        .map((project) => {
-          const { name, description, keywords, url } = project;
-          return stripIndent`
-          \\textbf{${name}} \\textit{${keywords.join(', ')}} \\hfill ${
-            url || ''
-          }
-          \\begin{itemize}
-            \\item ${description}
-          \\end{itemize}
-          \\vspace{2mm}
-        `;
-        })
-        .join('\n')}
-    `;
+    const projectsContent = projects
+      .map((project) => {
+        const { name, description, keywords, url } = project;
+        return stripIndent`
+        \\textbf{${name || ''}} \\textit{${
+          keywords ? keywords.join(', ') : ''
+        }} \\hfill ${url || ''}
+        \\begin{itemize}
+          \\item ${description || ''}
+        \\end{itemize}
+        \\vspace{2mm}
+      `;
+      })
+      .join('\n');
+    return projectsContent ? `\\section*{Projects}\n${projectsContent}` : '';
   };
-
-  // Generate sections using the defined methods
-  const profile = profileSection(data.basics);
-  const education = educationSection(data.education);
-  const work = workSection(data.work);
-  const skills = skillsSection(data.skills);
-  const projects = projectsSection(data.projects);
 
   // Construct the complete LaTeX document
   const document = stripIndent`
     \\documentclass[a4paper]{article}
     % ... any other packages or preamble code
 
+    % Remove page numbers
+    \\pagestyle{empty}
+
     \\begin{document}
-    ${profile}
-    ${education}
-    ${work}
-    ${skills}
-    ${projects}
-    % ... any other sections
+      ${profileSection(data.basics)}
+      ${educationSection(data.education)}
+      ${workSection(data.work)}
+      ${skillsSection(data.skills)}
+      ${projectsSection(data.projects)}
+      % ... any other sections
     \\end{document}
   `;
 

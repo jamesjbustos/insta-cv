9d6d6a7041b6b99aa3609b4f9e32651b0d88c547
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-11-17 23:08:24 -0500

    Form input ui component

--- insta-cv/client/src/components/FormSection.jsx
+++ insta-cv/client/src/components/FormSection.jsx
@@ -1,11 +1,22 @@
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 
-export default function FormSection({ id, label, type = 'text', placeholder }) {
+export default function FormSection({
+  id,
+  label,
+  type = 'text',
+  placeholder,
+  onChange,
+}) {
   return (
     <div className="flex flex-col gap-2">
       <Label htmlFor={id}>{label}</Label>
-      <Input id={id} type={type} placeholder={placeholder} />
+      <Input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
     </div>
   );
 }

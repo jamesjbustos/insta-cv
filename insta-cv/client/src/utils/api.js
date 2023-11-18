cf4cdcff78e8cd347409586a42de75103210c277
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-11-18 00:52:27 -0500

    add fetching resume by id on client api call

--- insta-cv/client/src/utils/api.js
+++ insta-cv/client/src/utils/api.js
@@ -17,3 +17,25 @@ export const deleteResume = async (resumeId) => {
   if (!response.ok) throw new Error('Failed to delete resume');
   return response.json();
 };
+
+export const fetchResumeById = async (resumeId) => {
+  const response = await fetch(
+    `http://localhost:3000/api/resumes/${resumeId}`,
+    {
+      credentials: 'include',
+    }
+  );
+  if (!response.ok) throw new Error('Failed to fetch resume');
+  return response.json();
+};
+
+export const saveResume = async (formData) => {
+  const response = await fetch('http://localhost:3000/api/resumes', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(formData),
+    credentials: 'include',
+  });
+  if (!response.ok) throw new Error('Failed to save resume');
+  return response.json();
+};
